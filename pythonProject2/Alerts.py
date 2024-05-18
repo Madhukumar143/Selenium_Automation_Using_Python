d86b1281aa6dd1a42f75e770c0228e332cd83e49
Madhu_kumar_HM <madhukumarhm123.com@gmail.com>
2024-05-18 17:40:32 +0530

    Change in Alerts.py

--- pythonProject2/Alerts.py
+++ pythonProject2/Alerts.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-
+#test Update
 datetime = datetime.now()
 print(datetime)
 ch_option =Options()
