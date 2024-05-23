d24c0154da69599de21b038589acaf018924c126
Madhu_kumar_HM <madhukumarhm123.com@gmail.com>
2024-05-23 13:34:37 +0530

    commit

---
+++ OOPs_Python/Modules/Inbuilt_Modules.py
@@ -0,0 +1,11 @@
+import math
+import random
+
+print(math.ceil(math.sqrt(7)))
+print(math.ceil(math.floor(7)))
+print(math.ceil(math.pi))
+
+l = ["RCB" , "LSG" ,"RR", "SRH" ]
+
+print(random.choice(l))
+print(random.randint(1,13))
